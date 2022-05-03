ded30402e943eae085c153f4b7fb56a14c982b68
Amonpan-suru <amonpan.suru@mail.kmutt.ac.th>
2022-05-03 17:43:29 +0700

    update 3D model

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+window.onload = pageLoad;
+
+function pageLoad(){
+  document.getElementById("defaultOpen").click();
+}
+
+function startGame(){
+	var allbox = document.querySelectorAll("#game-layer div");
+	if(allbox.length != 0 ){
+		clearScreen();
+	}
+	alert("Ready");
+	addBox();
+	timeStart();
+}
+
 function boxtothetop() {
     var windowTop = $(window)
       .scrollTop();
@@ -22,3 +38,20 @@ function boxtothetop() {
       .scroll(boxtothetop);
     boxtothetop();
   });
+
+  function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+ 
